3c1022c65638dd008e3c8b57bb16c3fc8df36d7d
GraysonWills <146792117+GraysonWills@users.noreply.github.com>
2025-06-07 19:56:21 -0400

    The User Class
    The User Class is INCREDIBLY simple that anyone could build it. I will
    have to rememeber to decode the password before loading it in

---
+++ User.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, username, password):
+        self._username = username
+        self.__password = password
+    
+    @property
+    def username(self):
+        return self._username
